00db94e3501de35344b81891f2ecdb3724b57e6a
Terence Lee <hone02@gmail.com>
2008-09-15 17:20:24 +0000

    * refactored tree walker for shoes test to use a helper method
    git-svn-id: svn+ssh://rose.makesad.us/home/hone/napkins_repos/trunk@35 6087afe7-7072-4dcf-84dc-23933bc9c4dc

--- test/test_walk_tree_shoes.rb
+++ test/test_walk_tree_shoes.rb
@@ -4,59 +4,38 @@ require 'lib/walk_tree_shoes'
 
 class TestWalkTreeShoes < Test::Unit::TestCase # :nodoc:
   def test_walk_root_first_line_testing
-    root_node = RootNode.new( ParagraphNode.new( TagNode.new( TextNode.new( "Testing" ) ) ) )
-
-    assert_equal "para( \"Testing\" )", WalkTreeShoes.walk_root( root_node )
+    test_tree_helper( "para( \"Testing\" )", "Testing" )
   end
 
   def test_walk_root_two_lines_hello_world
-    tokens = Scanner.scan( "Hello\n\nWorld" )
-    root_node = Parser.parse( tokens )
-    
-    assert_equal "para( \"Hello\" )\npara( \"World\" )", WalkTreeShoes.walk_root( root_node )
+    test_tree_helper( "para( \"Hello\" )\npara( \"World\" )", "Hello\n\nWorld" )
   end
 
   def test_walk_root_first_line_multiple_tags
-    tokens = Scanner.scan( "Hello World" )
-    root_node = Parser.parse( tokens )
-
-    assert_equal "para( \"Hello\", \" \", \"World\" )", WalkTreeShoes.walk_root( root_node )
+    test_tree_helper( "para( \"Hello\", \" \", \"World\" )", "Hello World" )
   end
 
   def test_walk_root_empty
-    tokens = Scanner.scan( "" )
-    root_node = Parser.parse( tokens )
-
-    assert_equal "", WalkTreeShoes.walk_root( root_node )
+    test_tree_helper( "", "" )
   end
 
   def test_walk_root_bold
-    tokens = Scanner.scan( "\n\n*bold*" )
-    root_node = Parser.parse( tokens )
-
-    assert_equal "para( strong( \"bold\" ) )", WalkTreeShoes.walk_root( root_node )
+    test_tree_helper( "para( strong( \"bold\" ) )", "\n\n*bold*" )
   end
 
   def test_walk_root_italics
-    tokens = Scanner.scan( "\n\n_italics_" )
-    root_node = Parser.parse( tokens )
-
-    assert_equal "para( em( \"italics\" ) )", WalkTreeShoes.walk_root( root_node )
+    test_tree_helper( "para( em( \"italics\" ) )", "\n\n_italics_" )
   end
 
   def test_walk_root_underscore
-    tokens = Scanner.scan( "\n\n+underscore+" )
-    root_node = Parser.parse( tokens )
-
-    assert_equal "para( ins( \"underscore\" ) )", WalkTreeShoes.walk_root( root_node )
+    test_tree_helper( "para( ins( \"underscore\" ) )", "\n\n+underscore+" )
   end
 
   def test_walk_root_subscript
-    root_node = Parser.parse( Scanner.scan( "\n\n~subscript~" ) )
-
-    assert_equal "para( sub( \"subscript\" ) )", WalkTreeShoes.walk_root( root_node )
+    test_tree_helper( "para( sub( \"subscript\" ) )", "\n\n~subscript~" )
   end
 
+  private
   def test_tree_helper( expected, input )
     assert_equal expected, WalkTreeShoes.walk_root( Parser.parse( Scanner.scan( input ) ) )
   end
